79154e3949900918265241c35f1557ec7471c4a3
Brenno Lima <brennocesar@live.com>
2024-08-15 11:13:39 -0400

    incomplete projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,29 @@
-import React, { useEffect } from 'react';
-import { Box, Container, Typography } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import {
+	Box,
+	Card,
+	Stack,
+	Divider,
+	Container,
+	Typography,
+	Collapse,
+	Grid,
+	IconButton,
+} from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import gsap from 'gsap';
 
 import { RocketSVG } from '../SVGComponents/RocketSVG';
 
 export const Projects = () => {
+	const [expanded, setExpanded] = useState([true, false, false]);
+
+	const handleExpandClick = (index) => {
+		var newState = [false, false, false];
+		newState[index] = !expanded[index];
+		setExpanded(newState);
+	};
+
 	useEffect(() => {
 		let float;
 		const activateDelay = (items) => {
@@ -13,7 +32,7 @@ export const Projects = () => {
 					`#float-${item}`,
 					{ y: '+=5px' },
 					{
-						y: '-=10px',
+						y: '-=25px',
 						yoyo: true,
 						repeat: -1,
 						ease: 'Power2.easeInOut',
@@ -43,18 +62,88 @@ export const Projects = () => {
 				PROJECTS
 			</Typography>
 			<RocketSVG width='13vw' />
-			<Box
+			<Card
 				sx={{
 					position: 'absolute',
 					right: '-5%',
-					top: '-5%',
+					bottom: '15%',
 					borderRadius: 1,
-					width: '500px',
+					width: '250px',
 					color: (theme) =>
 						theme.palette.getContrastText(theme.palette.background.paper),
 				}}
 				id='float-0'
-			></Box>
+			>
+				<Stack sx={{ alignItems: 'center' }}>
+					<Box
+						sx={{
+							width: '100%',
+							background: (theme) => theme.palette.background.paper,
+							display: 'flex',
+							justifyContent: 'center',
+							py: 1,
+						}}
+					>
+						<img src={'Github'} alt='github' />
+					</Box>
+					<Divider flexItem />
+					<Box sx={{ pt: 1 }}>
+						<Typography
+							align='center'
+							variant='subtitle2'
+							fontWeight={600}
+						></Typography>
+					</Box>
+					<Collapse
+						in={expanded[2]}
+						timeout='auto'
+						unmountOnExit
+						sx={{ width: '100%' }}
+					>
+						<Grid
+							container
+							direction='row'
+							alignItems='center'
+							justifyContent='space-around'
+							sx={{
+								py: 2,
+								px: 2,
+							}}
+							gap={2}
+						>
+							<Grid item sx={{ px: 1 }}>
+								<Typography variant='caption'>GitHub</Typography>
+							</Grid>
+							<Grid item sx={{ px: 1 }}>
+								<Typography variant='caption'>Jira</Typography>
+							</Grid>
+							<Grid item sx={{ px: 1 }}>
+								<Typography variant='caption'>Agile</Typography>
+							</Grid>
+							<Grid item sx={{ px: 1 }}>
+								<Typography variant='caption'>VS Code</Typography>
+							</Grid>
+							<Grid item sx={{ px: 1 }}>
+								<Typography variant='caption'>Postman</Typography>
+							</Grid>
+						</Grid>
+					</Collapse>
+					<IconButton
+						size='small'
+						onClick={() => handleExpandClick(2)}
+						aria-expanded={expanded[2]}
+						aria-label='show more'
+						sx={{
+							color: (theme) =>
+								theme.palette.getContrastText(theme.palette.background.paper),
+							transform: !expanded[2] ? 'rotate(0deg)' : 'rotate(180deg)',
+							transition: 'transform 0.2s linear',
+						}}
+					>
+						<ExpandMoreIcon />
+					</IconButton>
+				</Stack>
+			</Card>
 		</Container>
 	);
 };
